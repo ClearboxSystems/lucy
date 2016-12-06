eee9b6a5e114f6186519cb62c5184eac25a579ee
Paul Solomon <balderscape@clearboxsystems.com.au>
2016-12-06 18:32:27 +1100

    Lexer is working, but only with flex 2.5

--- src/parser/Lexer.cc
+++ src/parser/Lexer.cc
@@ -1,6 +1,7 @@
 #include "Lexer.hh"
 
-Lexer::Lexer(std::istream *in) : yyFlexLexer(in), yylval(nullptr) {}
+Lexer::Lexer(std::istream *in) : yyFlexLexer(in), yylval(nullptr) {
+}
 
 int Lexer::yylex(lucy::Parser::semantic_type *l_val) {
 	yylval = l_val;
--- src/parser/Parser.cc
+++ src/parser/Parser.cc
@@ -10,12 +10,13 @@ int parserRun(int argc, char **argv) {
 
 	std::string input;
     fprintf(stderr, "ready> ");
-    int i = 0;
+
     while (std::getline(std::cin, input)) {
         std::istringstream iss(input);
         Lexer lexer = Lexer(&iss);
 
-        while ((int tok = lexer.yylex(&tokval)) != lucy::Parser::token::END) {
+        int tok;
+        while ((tok = lexer.yylex(&tokval)) != lucy::Parser::token::END) {
         	printf("Lex: %d", tok);
         	if (tok == lucy::Parser::token::INTEGER)
 	        	printf("\t INT -> %d", tokval.ival);
@@ -24,5 +25,6 @@ int parserRun(int argc, char **argv) {
 
         fprintf(stderr, "ready> ");
     }
-
+    fprintf(stderr, "\n");
+    return 0;
 }
